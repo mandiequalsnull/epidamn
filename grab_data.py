e6b5eec2964606f5ac4ea881f21ee68682b69864
Johnny Perez <johnnypr11@gmail.com>
2020-03-29 16:48:00 -0500

    Grabs latest data from John Hopkins

---
+++ grab_data.py
@@ -0,0 +1,25 @@
+import requests
+from datetime import date,timedelta
+
+
+def main():
+    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/"
+    current_date = date.today()
+    yesterday = current_date - timedelta(days=1)
+    last_update = yesterday.strftime("%m-%d-%Y")
+    url =  url+'/' + str(last_update) + '.csv'
+    response = requests.get(url)
+    ## data is inside of response.text
+    print(response.text)
+
+def saveToFile(data,filename):
+    ##Write something to save the data to either json or csv
+    ## Use the date as the file name so like this: 2020-03-28.csv or 2020-03-28.json 
+    pass
+
+def cleanData(data):
+    ## Might want to clean the data before you save it so something here
+    pass
+
+if __name__ == "__main__":
+    main()
